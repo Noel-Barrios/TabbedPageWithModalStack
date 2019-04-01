d1429bb63b759f693270f6b0c4a33e815fef17b8
Barrios <nbarrio4@ms.ds.uhc.com>
2019-04-01 13:46:57 -0500

    Add project files.

---
+++ TabbedPageWithModals/TabbedPageWithModals/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TabbedPageWithModals.Models;
+
+namespace TabbedPageWithModals.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
