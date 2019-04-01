97b9c0d9e47c8d2af94d04876f9b4796668f86c9
Noel Barrios <noel.barrios@optum.com>
2019-04-01 15:35:54 -0500

    added button that serves up a webview (browser)

---
+++ TabbedPageWithModals/TabbedPageWithModals/ViewModels/LogoutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TabbedPageWithModals.ViewModels
+{
+    public class LogoutViewModel : BaseViewModel
+    {
+        public ICommand OpenWebCommand { get; }
+
+        public LogoutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://www.google.com/")));
+        }
+    }
+}
--- TabbedPageWithModals/TabbedPageWithModals/Views/LogoutPage.xaml.cs
+++ TabbedPageWithModals/TabbedPageWithModals/Views/LogoutPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using TabbedPageWithModals.ViewModels;
 using Xamarin.Forms;
 
 namespace TabbedPageWithModals.Views
